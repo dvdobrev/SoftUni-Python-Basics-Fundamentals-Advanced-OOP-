48b4e99e5e88a5aef2c95c6f214be474ec729cfc
Dobrin Dobrev <dobrin.valev.dobrev@gmail.com>
2021-06-10 01:02:40 +0200

    add the Problem 07 - Guild System. See the description in the guild file.

---
+++ Python Advanced/Python_ADVANCED_OOP_MODUL/Defining_Classes_01/EXERCISES/project_problem_07/player.py
@@ -0,0 +1,27 @@
+class Player:
+    def __init__(self, name: str, hp: int, mp: int):
+        self.name = name
+        self.hp = hp
+        self.mp = mp
+        self.skills = {}
+        self.guild = "Unaffiliated"
+
+    def add_skill(self, skill_name, mana_cost):
+        if skill_name in self.skills:
+            return f"Skill already added"
+
+        else:
+            self.skills[skill_name] = mana_cost
+            return f"Skill {skill_name} added to the collection of the player {self.name}"
+
+    def player_info(self):
+        res = f"Name: {self.name}\n" \
+              f"Guild: {self.guild}\n" \
+              f"HP: {self.hp}\n" \
+              f"MP: {self.mp}\n"
+        for s, m in self.skills.items():
+            res += f"==={s} - {m}\n"
+
+        return res
+
+
