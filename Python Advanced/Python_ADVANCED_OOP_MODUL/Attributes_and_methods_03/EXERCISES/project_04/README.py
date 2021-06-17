4ac9249b3158682da27211414238a1689a8a34d8
Dobrin Dobrev <dobrin.valev.dobrev@gmail.com>
2021-06-18 01:28:00 +0200

    add project 4 from Exercises Attributes and Methods

---
+++ Python Advanced/Python_ADVANCED_OOP_MODUL/Attributes_and_methods_03/EXERCISES/project_04/README.py
@@ -0,0 +1,42 @@
+"""
+аз за всеки един файл без init и gym го бях направил така обаче judge ми даваше 95 точки:
+
+class Customer:
+    _id = 1     ТУК СЛАГАМ ДА ПОЧВА ОТ 1 (КАКТО Е ПО УСЛОВИЕ)
+
+    def __init__(self, name: str, address: str, email: str):
+        self.name = name
+        self.address = address
+        self.email = email
+        self.id = Customer._id
+        Customer._id += 1      ТУК УВЕЛИЧАВАМ ИД СЛЕД КАТО Е СЪЗДАДЕНА ВЕЧЕ ЕДНА ИНСТАНЦИЯ
+
+
+    def __repr__(self):
+        return f"Customer <{self.id}> {self.name}; Address: {self.address}; Email: {self.email}"
+
+    @staticmethod
+    def get_next_id():
+        return Customer._id    ТУК ПРОСТО ВРЪЩАМ ИД, КОЕТО АКО НЕ Е ИМАЛО ИНСТАНЦИЯ ЩЕ Е 1, АКО Е МИНАЛА ИНСТАНЦИЯ ЩЕ Е 2 И Т.Н.
+
+
+"""
+
+class Customer:
+    _id = 0
+
+    def __init__(self, name: str, address: str, email: str):
+        Customer._id += 1
+        self.name = name
+        self.address = address
+        self.email = email
+        self.id = Customer._id
+
+
+    def __repr__(self):
+        return f"Customer <{self.id}> {self.name}; Address: {self.address}; Email: {self.email}"
+
+    @staticmethod
+    def get_next_id():
+        return Customer._id + 1
+
