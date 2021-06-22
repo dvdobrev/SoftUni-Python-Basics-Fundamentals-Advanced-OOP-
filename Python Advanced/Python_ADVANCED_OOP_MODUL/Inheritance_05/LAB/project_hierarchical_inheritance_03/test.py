0b257a0b870714a3878232112d72008c7d0fa7ce
Dobrin Dobrev <dobrin.valev.dobrev@gmail.com>
2021-06-22 22:31:40 +0200

    add test file

---
+++ Python Advanced/Python_ADVANCED_OOP_MODUL/Inheritance_05/LAB/project_hierarchical_inheritance_03/test.py
@@ -0,0 +1,12 @@
+from project.dog import Dog
+from project.animal import Animal
+from project.cat import Cat
+
+animal = Animal()
+dog = Dog()
+cat = Cat()
+
+print(dog.eat())
+print(dog.bark())
+print(cat.meow())
+print(cat.eat())
