f3c78751c3b95c84d63dcb568ff6e49ebb7275f0
Dobrin Dobrev <dobrin.valev.dobrev@gmail.com>
2021-07-02 21:40:58 +0200

    add some changes

--- Python Advanced/Python_ADVANCED_OOP_MODUL/Defining_Classes_01/EXERCISES/Steam_user_04.py
+++ Python Advanced/Python_ADVANCED_OOP_MODUL/Defining_Classes_01/EXERCISES/Steam_user_04.py
@@ -18,13 +18,14 @@ class SteamUser:
 
         return f"{game} is already in your library"
 
-    def stats(self):
+    def status(self):
         return f"{self.username} has {len(self.games)} games. Total play time: {self.played_hours}"
 
-# user = SteamUser("Peter", ["Rainbow Six Siege", "CS:GO", "Fortnite"])
-# print(user.play("Fortnite", 3))
-# print(user.play("Oxygen Not Included", 5))
-# print(user.buy_game("CS:GO"))
-# print(user.buy_game("Oxygen Not Included"))
-# print(user.play("Oxygen Not Included", 6))
-# print(user.stats())
+
+user = SteamUser("Peter", ["Rainbow Six Siege", "CS:GO", "Fortnite"])
+print(user.play("Fortnite", 3))
+print(user.play("Oxygen Not Included", 5))
+print(user.buy_game("CS:GO"))
+print(user.buy_game("Oxygen Not Included"))
+print(user.play("Oxygen Not Included", 6))
+print(user.stats())
